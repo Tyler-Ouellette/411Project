78fc4270846fc6ef459ec55da3b2648b2a4dddf6
tyler-ouellette <ouell117@uwindsor.ca>
2020-03-24 22:23:16 -0400

    GetAuthUser Working!

--- Tests/API Tests/Users Tests/GetAuthUser.test.js
+++ Tests/API Tests/Users Tests/GetAuthUser.test.js
@@ -1,19 +1,11 @@
-fetch(get, 'http://localhost:5000/api/auth', config);
-
 global.fetch = require('jest-fetch-mock');
-
-const config = {
-    headers: {
-        'X-Auth-Token': process.env.XAuthToken,
-    },
-};
-
 test('Get Auth User', () => {
-    fetch.mockResponseOnce(JSON.stringify({
-        user: {
-            'X-Auth-Token': `${process.env.XAuthToken}`
-        }
-    }));
-    
-    https://documenter.getpostman.com/preview/10827403-f5c28f78-9415-4ec7-9fd8-7da385082577?versionTag=latest&apiName=CURRENT&version=latest&top-bar=ffffff&right-sidebar=303030&highlight=ef5b25
-}))
+    // fetch(get, 'http://localhost:5000/api/auth', config);
+    fetch.mockResponseOnce(
+        JSON.stringify({
+            user: {
+                'X-Auth-Token': `${process.env.XAuthToken}`,
+            },
+        })
+    );
+});
